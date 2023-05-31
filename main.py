2d46c3e6f059ce12cd0038d9311284da6da58b90
Aaron <sapblatt11@gmail.com>
2023-05-31 12:46:07 -0400

    Added test plot func. Moved data processing to process_data func

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def is_header(entry):
     if '@' in entry[0]:  # All varities of SAM headers begin with a @ followed by 2 char
         return True
 
-
-def main():
+def process_data():
     file = 'Data/PB644_EB817.hifi_reads.sam'
 
     with open(file, 'r') as infile:
@@ -21,7 +20,7 @@ def main():
         for line in infile:
 
             match lineCount:
-                case 10000:
+                case 2:
                     break  # Avoid reading the whole file, for now
 
                 case _:
@@ -36,11 +35,24 @@ def main():
                             
                             #Find overlapping sequences
                             sequences = get_counts_from_seq(alignment.SEQ, k=50)
+                            #sequences = get_counts_from_seq("ATGGTATGTA", 3)
                             edges = get_edges(sequences)
-                            
+
                             toyplot.browser.show(plot_debruijn_graph(edges)[0])
 
             lineCount += 1
+
+def test_plot(sequence, k):
+    #Circular Sequence example: test_plot("ACTGAGTACCATGGAC",4)
+    sequences = get_counts_from_seq(sequence, k, False)
+    edges = get_edges(sequences)
+    print(sequences)
+    print(edges)
+    toyplot.browser.show(plot_debruijn_graph(edges, width=1000, height=1000)[0])
+
+def main():
+    #process_data()    
+    test_plot("ACTGAGTACCATGGAC",4)
 
 
 if __name__ == "__main__":
