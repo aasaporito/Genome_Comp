dc7733493fcc6c985d3bb5be994168046a57833e
Aaron <sapblatt11@gmail.com>
2023-05-31 12:05:28 -0400

    Added BeBruijn helper functions

---
+++ DeBruijn.py
@@ -0,0 +1,70 @@
+import toyplot
+
+''' 
+Split a sequence into parts of length k and return them with their 
+respect counts.
+
+Parameters: 
+	- seq: an input string
+	- k: chunk sizes to test for overlap
+Returns a dictionary.
+'''
+
+
+def get_counts_from_seq(seq, k=50, circular=True):
+    collection = {}  # dict
+
+    for i in range(0, len(seq)):
+        chunk = seq[i:i + k]  # Gets the next sequence chunk between i and k
+
+        chunk_length = len(chunk)
+
+        # For sequences that ideally wrap around
+        if circular:
+            if chunk_length != k:
+                chunk += seq[:(k - chunk_length)]
+
+        # Linear sequences
+        else:
+            if chunk_length != k:
+                continue
+
+        if chunk in collection:
+            collection[chunk] += 1  # Already exists in collection
+        else:
+            collection[chunk] = 1  # New entry to the collection
+
+    return collection
+
+
+'''
+'''
+def get_edges(collection):
+
+    edges = set()
+
+    for j in collection:
+        for k in collection:
+            if j != k:
+                if j[1:] == k[:-1]:
+                    edges.add((j[:-1], k[:-1]))  # Add overlap from j to k
+                if j[:-1] == k[:-1]:
+                    edges.add((k[:-1], j[:-1]))  # Add overlap from k to j
+
+    return edges
+
+
+def plot_debruijn_graph(edges, width=4000, height=4000):
+    "returns a toyplot graph from an input of edges"
+    graph = toyplot.graph(
+        [i[0] for i in edges],
+        [i[1] for i in edges],
+        width=width,
+        height=height,
+        tmarker=">",
+        vsize=25,
+        vstyle={"stroke": "black", "stroke-width": 2, "fill": "none"},
+        vlstyle={"font-size": "0px"},
+        estyle={"stroke": "black", "stroke-width": 2},
+        layout=toyplot.layout.FruchtermanReingold(edges=toyplot.layout.CurvedEdges()))
+    return graph
