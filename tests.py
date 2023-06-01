e111c2a5a95e44d16f72eab10e3116db05c7454e
Aaron <sapblatt11@gmail.com>
2023-06-01 15:43:19 -0400

    Added docstring to tests.py - not usable

--- tests.py
+++ tests.py
@@ -1,4 +1,15 @@
+from DeBruijn import *
+
 def test_plot(sequence, k):
+    """Summary
+        Non-functional method for testing hand-input data.
+    Args:
+        sequence (str): Description
+        k (int): Description
+    
+    Returns:
+        DiGraph: Description
+    """
     #Circular Sequence example: test_plot("ACTGAGTACCATGGAC",4)
     sequences = get_counts_from_seq(sequence, k)
     edges = get_edges(sequences)
